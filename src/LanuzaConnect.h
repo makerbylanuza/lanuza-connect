bd79205a260246fa19198a0c62c63c0480951052
makerbylanuza <makerspace@juandelanuza.org>
2025-03-24 16:39:41 +0100

    Update LanuzaConnect.h

--- src/LanuzaConnect.h
+++ src/LanuzaConnect.h
@@ -1,32 +1,34 @@
 #ifndef LANUZACONNECT_H
 #define LANUZACONNECT_H
 
+// Include libraries
 #include <esp_now.h>
 #include <WiFi.h>
 
-// Tipo de datos para el mensaje
+// Data type for the message
 typedef struct struct_message {
-    char data[250];
+  char data[250];
 } struct_message;
 
 class LanuzaConnect {
 public:
-    LanuzaConnect(const String &mode);
-
-    void initializeWiFi();
-    void init();
-    void addPeer(const uint8_t *address);    
-    void sendString(const String &text, const uint8_t *destAddress);
-    String getLast();
+  // Constructor
+  LanuzaConnect(const String &mode);
     
-    // Métodos estáticos para callbacks
-    static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-    static void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len);
+  // Available functions
+  void initializeWiFi();
+  void init();
+  void addPeer(const uint8_t *address);    
+  void sendString(const String &text, const uint8_t *destAddress);
+  String getLast();
+  
+  // Static callback methods
+  static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+  static void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len);
 
 private:
-    static void printMAC(const uint8_t *macAddr);
-    String _mode;
-    static String lastReceived;
+  static void printMAC(const uint8_t *macAddr);
+  String _mode;
+  static String lastReceived;
 };
-
 #endif
