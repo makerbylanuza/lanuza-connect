cef0b4b98c5444915a8310161d4a55a7f9fb63c2
makerbylanuza <makerspace@juandelanuza.org>
2025-03-24 16:30:40 +0100

    Update multitransmitter.ino

--- examples/multitransmitter/multitransmitter.ino
+++ examples/multitransmitter/multitransmitter.ino
@@ -2,23 +2,23 @@
 
 LanuzaConnect esp("TRANSMITTER");
 
-// Dirección MAC de los receptores (sustituir por las correctas)
-uint8_t receiver1[] = {0xB8, 0xD6, 0x1A, 0x47, 0xC3, 0x3C};
-uint8_t receiver2[] = {0xB8, 0xD6, 0x1A, 0x47, 0xC3, 0x3C};
+// Both receivers' MAC addresses (must be changed to actual addresses)
+const uint8_t receiver1[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB};
+const uint8_t receiver2[] = {0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67};
 
 
 void setup() {
-    Serial.begin(115200);
-    esp.init();
-    esp.addPeer(receiver1);
-    esp.addPeer(receiver2);
+  Serial.begin(115200);
+  esp.init();
+  esp.addPeer(receiver1);
+  esp.addPeer(receiver2);
 }
 
 void loop() {
-   esp.sendString("Hola, receptor 1", receiver1);
+   esp.sendString("Hello, receiver #1!", receiver1);
    delay(1000);
-   esp.sendString("Hola, receptor 2", receiver2);
-   delay(5000);
+   esp.sendString("Hello, receiver #2!", receiver2);
+   delay(4000);
 }
 
 
